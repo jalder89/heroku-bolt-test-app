a0926262ad7f27e24a78b2c3ad290d2ebd614852
William Barnes <wbarnes@wbarnes-ltmpcwh.internal.salesforce.com>
2021-11-16 11:55:51 -0500

    fixed typo in process

--- tools.js
+++ tools.js
@@ -3,7 +3,7 @@ const http = require('http');
 const startListener = () => {
     handle = (req, res) => console.log(req)
     server = http.createServer(handle)
-    server.listen(proccess.env.PORT || 5000) 
+    server.listen(process.env.PORT || 5000) 
 };
 
 module.exports = {
