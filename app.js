0058d4fe2052d485fbfedf7b6d6777694a5db083
William Barnes <wbarnes@wbarnes-ltmpcwh.internal.salesforce.com>
2021-11-16 10:02:21 -0500

    log port generated to env var

--- app.js
+++ app.js
@@ -47,5 +47,5 @@ app.action('button_click', async ({ body, ack, say }) => {
   // Start your app
   await app.start(process.env.PORT || 3000);
 
-  console.log('⚡️ Bolt app is running!');
+  console.log('⚡️ Bolt app is running! ' + process.env.PORT);
 })();
